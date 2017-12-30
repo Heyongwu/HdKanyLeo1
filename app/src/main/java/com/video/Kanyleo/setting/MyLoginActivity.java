76c45a56c5d564a4e34947d50159797c7d48b235
Dangzt <596834980@qq.com>
2017-12-30 10:41:27 +0800

    my隐藏标题栏

--- app/src/main/java/com/video/Kanyleo/setting/MyLoginActivity.java
+++ app/src/main/java/com/video/Kanyleo/setting/MyLoginActivity.java
@@ -2,6 +2,7 @@ package com.video.Kanyleo.setting;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,8 @@ public class MyLoginActivity extends AppCompatActivity implements IloginActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_login);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         initView();
         presenter = new LoginPresenter(this);
 
